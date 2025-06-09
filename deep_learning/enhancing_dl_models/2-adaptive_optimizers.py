c5875fa2ebb17ac823d7f6567660f4c4a7289bf8
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-09 19:41:26 +0100

    a

--- deep_learning/enhancing_dl_models/2-adaptive_optimizers.py
+++ deep_learning/enhancing_dl_models/2-adaptive_optimizers.py
@@ -5,6 +5,7 @@ Task 2
 
 from tensorflow import keras
 
+
 def get_optimizer(name, learning_rate, momentum, beta_1, beta_2, rho):
     """
     Returns a Keras optimizer based on the given name.
@@ -13,16 +14,19 @@ def get_optimizer(name, learning_rate, momentum, beta_1, beta_2, rho):
     - name (str): Optimizer name — 'sgd', 'adam', or 'rmsprop'.
     - learning_rate (float): Learning rate for the optimizer.
     - momentum (float): Momentum value (only used for SGD).
-    - beta_1 (float): Exponential decay rate for the 1st moment estimate (only used for Adam).
-    - beta_2 (float): Exponential decay rate for the 2nd moment estimate (only used for Adam).
+    - beta_1 (float): Exponential decay rate for the 1st moment estimate
+    - beta_2 (float): Exponential decay rate for the 2nd moment estimate
     - rho (float): Decay factor for RMSProp (only used for RMSProp).
 
     Returns:
     - optimizer: A tf.keras.optimizers.Optimizer instance.
     """
     if name == 'sgd':
-        return keras.optimizers.SGD(learning_rate=learning_rate, momentum=momentum)
+        return keras.optimizers.SGD(learning_rate=learning_rate,
+                                    momentum=momentum)
     if name == 'adam':
-        return keras.optimizers.Adam(learning_rate=learning_rate, beta_1=beta_1, beta_2=beta_2)
+        return keras.optimizers.Adam(learning_rate=learning_rate,
+                                     beta_1=beta_1, beta_2=beta_2)
     if name == 'rmsprop':
-        return keras.optimizers.RMSprop(learning_rate=learning_rate, rho=rho)
+        return keras.optimizers.RMSprop(learning_rate=learning_rate,
+                                        rho=rho)
