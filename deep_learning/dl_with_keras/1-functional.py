d1d30281fce353e45f4b8c4b6845637e326e4409
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-12 12:07:25 +0100

    a

--- deep_learning/dl_with_keras/1-functional.py
+++ deep_learning/dl_with_keras/1-functional.py
@@ -14,8 +14,10 @@ def build_model(input_dim, neurons_h):
     model -- keras model
     """
     input_layer = keras.layers.Input(shape=(input_dim,))
-    hidden_layer = keras.layers.Dense(neurons_h, activation='sigmoid')(input_layer)
-    output_layer = keras.layers.Dense(10, activation='softmax')(hidden_layer)
+    hidden_layer = keras.layers.Dense(neurons_h,
+                                      activation='sigmoid')(input_layer)
+    output_layer = keras.layers.Dense(10,
+                                      activation='softmax')(hidden_layer)
 
     model = keras.Model(inputs=input_layer, outputs=output_layer)
 
