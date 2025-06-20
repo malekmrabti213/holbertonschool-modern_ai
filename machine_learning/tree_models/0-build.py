2665f72d6ce6d10cae499dc54a1ea418cd530ebd
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-20 16:25:06 +0100

    b

---
+++ machine_learning/tree_models/0-build.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Task 0
+"""
+from sklearn.tree import DecisionTreeClassifier
+
+
+def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
+    """
+    """
+    clf = DecisionTreeClassifier(criterion='gini', max_depth=None,
+                                 min_samples_leaf=min_samples_leaf,
+                                 min_samples_split=min_samples_split,
+                                 random_state=random_state)
+    return clf
---
+++ machine_learning/tree_models/README.md
@@ -0,0 +1,1 @@
+This is the Tree-based models project.
