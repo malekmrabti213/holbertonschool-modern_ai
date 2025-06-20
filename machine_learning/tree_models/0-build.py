2ffae22aefd112ca729ea7c7ffa401b3049f7cbc
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-20 17:03:20 +0100

    b

--- machine_learning/tree_models/0-build.py
+++ machine_learning/tree_models/0-build.py
@@ -7,14 +7,6 @@ from sklearn.tree import DecisionTreeClassifier
 
 def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
     """
-    Builds and returns a DecisionTreeClassifier with the parameters.
-
-    The classifier uses:
-    - Gini impurity as the criterion for split quality
-    - No maximum depth limit (max_depth=None)
-    - Custom values for min_samples_leaf and min_samples_split
-    - A fixed random_state for reproducibility
-
     Arguments:
         min_samples_leaf (int): samples required at a leaf node.
         min_samples_split (int): samples required to split an internal node.
