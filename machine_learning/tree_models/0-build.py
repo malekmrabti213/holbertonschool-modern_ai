64605c576f33ab390b5077504253b5fdc1c412ef
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-20 16:55:42 +0100

    b

--- machine_learning/tree_models/0-build.py
+++ machine_learning/tree_models/0-build.py
@@ -7,7 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 def build_decision_tree(min_samples_leaf, min_samples_split, random_state):
     """
-    
+    build_decision_tree
     """
     clf = DecisionTreeClassifier(criterion='gini', max_depth=None,
                                  min_samples_leaf=min_samples_leaf,
