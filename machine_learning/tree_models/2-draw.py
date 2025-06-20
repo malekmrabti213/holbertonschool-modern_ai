926072994e5535bdf3c481f1ab419b5f98b23b7c
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-20 23:49:26 +0100

    b

--- machine_learning/tree_models/2-draw.py
+++ machine_learning/tree_models/2-draw.py
@@ -5,14 +5,14 @@ Task 2
 from sklearn import tree
 
 
-def draw(clf, feature_names,class_names):
+def draw(clf, feature_names, class_names):
     """
     Displays the textual representation of a trained Decision Tree.
 
     Parameters:
-        clf (DecisionTreeClassifier): Trained decision tree classifier.
-        feature_names (list): feature names.
-        class_names (list): target classes  
+        clf (DecisionTreeClassifier): Trained decision tree classifier
+        feature_names (list): feature names
+        class_names (list): target classes 
     """
     tree_text = tree.export_text(clf, feature_names=feature_names,
                                  class_names=class_names)
