780dde4276aceedd60dc2fc9ac152026d7e2d3b5
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-20 23:50:05 +0100

    b

--- machine_learning/tree_models/2-draw.py
+++ machine_learning/tree_models/2-draw.py
@@ -12,7 +12,7 @@ def draw(clf, feature_names, class_names):
     Parameters:
         clf (DecisionTreeClassifier): Trained decision tree classifier
         feature_names (list): feature names
-        class_names (list): target classes 
+        class_names (list): target classes
     """
     tree_text = tree.export_text(clf, feature_names=feature_names,
                                  class_names=class_names)
