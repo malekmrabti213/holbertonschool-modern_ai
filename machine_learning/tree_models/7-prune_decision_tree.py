12934e7ba4cbba44a24586feb63b4007f380bc1e
malekmrabti213 <malek6mrabti@gmail.com>
2025-06-27 17:27:59 +0100

    d

---
+++ machine_learning/tree_models/7-prune_decision_tree.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Task 7
+"""
+from sklearn import tree
+
+
+def prune_and_evaluate_trees(X_train, y_train, X_test,
+                             y_test, ccp_alphas,
+                             random_state, min_samples_leaf,
+                             min_samples_split):
+    """
+    Parameters:
+        X_train, y_train: Training data and labels
+        X_test, y_test: Testing data and labels
+        ccp_alphas (ndarray): Array of ccp_alpha values
+        random_state (int): Seed for reproducibility
+        min_samples_leaf(int):Minimum samples per leaf
+        min_samples_split(int):Minimum samples required to split a node
+
+    Returns:
+        clfs (list): List of trained DecisionTreeClassifier instances
+        train_scores (list): Training accuracy scores for each model
+        test_scores (list): Testing accuracy scores for each model
+    """
+    clfs = []
+    for ccp_alpha in ccp_alphas:
+        clf = tree.DecisionTreeClassifier(ccp_alpha=ccp_alpha,
+                                     random_state=random_state,
+                                     min_samples_leaf=min_samples_leaf,
+                                     min_samples_split=min_samples_split)
+        clf.fit(X_train, y_train)
+        clfs.append(clf)
+
+    train_scores = [clf.score(X_train, y_train) for clf in clfs]
+    test_scores = [clf.score(X_test, y_test) for clf in clfs]
+
+    return clfs, train_scores, test_scores
