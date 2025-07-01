40c817a0bd761b99ac78ffca20f67b352069779c
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-01 12:21:33 +0100

    c

--- machine_learning/linear_models/1-regression_evaluation_metrics.py
+++ machine_learning/linear_models/1-regression_evaluation_metrics.py
@@ -5,6 +5,7 @@ Task 1
 from sklearn import metrics
 import numpy as np
 
+
 def evaluation_metrics_for_regression(y_true, y_pred):
     """
     Computes standard evaluation metrics for regression.
@@ -20,5 +21,5 @@ def evaluation_metrics_for_regression(y_true, y_pred):
     rmse = np.sqrt(mse)
     mae = metrics.mean_absolute_error(y_true, y_pred)
     r2 = metrics.r2_score(y_true, y_pred)
-    
+
     return mse, rmse, mae, r2
