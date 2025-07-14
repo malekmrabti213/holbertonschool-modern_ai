d748edcd5727218daa36f3058f3dd03f09e2fd3b
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-14 18:03:28 +0100

    c

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -3,13 +3,12 @@
 Task 1
 """
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def plot_missingness(df):
     """
     """
-    plt.figure(figsize=(8,4))
+    plt.figure(figsize=(12,8))
     plt.imshow(df.isnull().T, aspect='auto')
     plt.yticks(range(len(df.columns)), df.columns)
     plt.title('Missingness Heatmap')
