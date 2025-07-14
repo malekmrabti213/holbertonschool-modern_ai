147d3e25743e0cdafb53fafdd7882e088e2dabec
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-14 21:23:35 +0100

    c

---
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Task 3
+"""
+
+
+def clean_total_charges(df, method='drop'):
+    """
+    """
+    df = df.copy()
+    if method=='drop':
+        df = df.dropna(subset=['TotalCharges'])
+    elif method=='median':
+        df.fillna({'TotalCharges': df['TotalCharges'].median()}, inplace=True)
+    else:
+        mask = df['TotalCharges'].isna()
+        df.loc[mask, 'TotalCharges'] = (
+            df.loc[mask, 'MonthlyCharges'] * df.loc[mask, 'tenure'])
+    return df
