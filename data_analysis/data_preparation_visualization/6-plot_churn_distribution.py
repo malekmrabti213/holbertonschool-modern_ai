87f2c767a74206d6c5a1041495187b6bf62d610e
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-14 23:20:01 +0100

    c

---
+++ data_analysis/data_preparation_visualization/6-plot_churn_distribution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Task 6
+"""
+import matplotlib.pyplot as plt
+
+
+def plot_churn_distribution(df):
+    """
+    """
+    plt.figure(figsize=(12, 8))
+    counts = df['Churn'].value_counts()
+    plt.bar(counts.index, counts.values, color=['skyblue','salmon'])
+    plt.title('Churn Distribution')
+    plt.ylabel('Count')
+    plt.show()
