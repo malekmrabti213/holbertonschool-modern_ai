cf3c01dcb834139c881af266ceae85e2f927243f
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-14 18:07:59 +0100

    c

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 def plot_missingness(df):
     """
     """
-    plt.figure(figsize=(12,8))
+    plt.figure(figsize=(12, 8))
     plt.imshow(df.isnull().T, aspect='auto')
     plt.yticks(range(len(df.columns)), df.columns)
     plt.title('Missingness Heatmap')
