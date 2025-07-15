252cdef0302bf5497d4b7452e9dc7fe62d9e7375
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-15 16:38:45 +0100

    c

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -5,9 +5,10 @@ Task 2
 import pandas as pd
 
 
-def convert_total_charges(df):
+def convert_columns(df):
     """
     """
     df = df.copy()
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['SeniorCitizen'] = df['SeniorCitizen'].astype('object')
     return df
