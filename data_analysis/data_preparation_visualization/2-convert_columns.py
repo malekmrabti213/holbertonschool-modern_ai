354769504b447f0064885113fcebda335efc7cf9
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-15 18:56:46 +0100

    c

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -10,5 +10,5 @@ def convert_columns(df):
     """
     df = df.copy()
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
-    df['SeniorCitizen'] = df['SeniorCitizen'].astype('object')
+    df['SeniorCitizen'] = df['SeniorCitizen'].map({0: 'No', 1: 'Yes'})
     return df
