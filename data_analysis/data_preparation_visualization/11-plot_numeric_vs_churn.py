d3e978f4b6b080b93271fc2a1b8fd430d9207084
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-17 16:05:53 +0100

    c

--- data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
+++ data_analysis/data_preparation_visualization/11-plot_numeric_vs_churn.py
@@ -9,9 +9,9 @@ def plot_numeric_vs_churn(df, col):
     """
     """
     plt.figure(figsize=(12, 8))
-    yes = df[df['Churn']=='Yes'][col].dropna()
-    no  = df[df['Churn']=='No'][col].dropna()
-    plt.hist([no, yes], bins=30, label=['No','Yes'])
+    yes = df[df['Churn'] == 'Yes'][col].dropna()
+    no  = df[df['Churn'] == 'No'][col].dropna()
+    plt.hist([no, yes], bins = 30, label=['No', 'Yes'])
     plt.title(f'{col} Distribution by Churn')
     plt.xlabel(col)
     plt.legend(title='Churn')
