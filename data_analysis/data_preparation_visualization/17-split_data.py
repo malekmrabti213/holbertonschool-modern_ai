e7ab7f1369b27bc1b2b6d2648d8fdb39bf3f9e77
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-19 02:26:27 +0100

    c

--- data_analysis/data_preparation_visualization/17-split_data.py
+++ data_analysis/data_preparation_visualization/17-split_data.py
@@ -8,9 +8,10 @@ from sklearn import model_selection
 def split_data(df, target='Churn', test_size=0.2, random_state=42):
     """
     """
+    df = df.copy()
     X = df.drop(columns=[target])
     y = df[target]
     return model_selection.train_test_split(X, y,
-                            test_size=test_size,
-                            stratify=y,
-                            random_state=random_state)
+                                            test_size=test_size,
+                                            stratify=y,
+                                            random_state=random_state)
