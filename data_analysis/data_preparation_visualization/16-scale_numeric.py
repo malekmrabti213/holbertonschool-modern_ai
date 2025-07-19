7239c75d44490b1b59ddec07bdf416c2ab9ce7e6
malekmrabti213 <malek6mrabti@gmail.com>
2025-07-19 01:57:15 +0100

    c

--- data_analysis/data_preparation_visualization/16-scale_numeric.py
+++ data_analysis/data_preparation_visualization/16-scale_numeric.py
@@ -10,6 +10,6 @@ def scale_numeric(df):
     """
     df = df.copy()
     scaler = preprocessing.StandardScaler()
-    num_cols = ['MonthlyCharges','TotalCharges']
+    num_cols = ['MonthlyCharges', 'TotalCharges']
     df[num_cols] = scaler.fit_transform(df[num_cols])
     return df
